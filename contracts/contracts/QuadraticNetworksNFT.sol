56fa87d5ff199448bcd63047df63ac680899f1a6
Brian Li <brian.li.101@gmail.com>
2023-11-10 11:11:54 +0300

    hardhat setup

--- contracts/contracts/QuadraticNetworksNFT.sol
+++ contracts/contracts/QuadraticNetworksNFT.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract QuadraticNetworksNFT is ERC721, IERC721Enumerable, Ownable {
-
-    using SafeMath for uint256;
+contract QuadraticNetworksNFT is ERC721, ERC721Enumerable, Ownable {
 
     mapping(address => address) public _nominations;
     uint256 public _ownerCount;
