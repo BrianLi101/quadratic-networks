f2dab1167c64f6df0ea8ceef8d2bdfd3d90bafec
Jack Yeh <jckyeh@gmail.com>
2023-11-11 01:43:01 +0300

    Add create group form

--- frontend/src/app/group/new/page.tsx
+++ frontend/src/app/group/new/page.tsx
@@ -1,21 +1,59 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function NewGroup() {
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    try {
+      const response = true;
+      const id = "1";
+
+      if (response) {
+        console.log("success submit");
+
+        // TODO: Route to group page
+        router.push(`/group/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 lg:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 lg:p-24 bg-[#19473F]">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Create new quadratic group</h1>
-        {/* TODO: Add form */}
-        <p>Name</p>
-        <p>Upload image</p>
-        <p>Max group size</p>
-        <p>Chain</p>
-        <Link
-          href="/dashboard"
+        <h1 className="text-2xl">Create new quadratic group</h1>
+        <form onSubmit={handleSubmit} className="mt-6">
+          <div className="flex flex-col">
+            <label htmlFor="groupName">Group name:</label>
+            <input
+              type="text"
+              id="groupName"
+              className="py-3 px-4 rounded bg-green-800 mt-2 mb-6"
+              name="groupName"
+              placeholder="E.g. Secret society"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="groupImage">Upload image:</label>
+            <input
+              type="file"
+              id="groupImage"
+              name="groupImage"
+              className="mt-2 mb-6"
+            />
+          </div>
+        </form>
+        <button
+          type="submit"
+          onClick={handleSubmit}
           className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         >
           Create group
-        </Link>
+        </button>
       </div>
     </main>
   );
