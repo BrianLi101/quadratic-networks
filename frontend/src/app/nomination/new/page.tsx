43dff101e2c22bbb4b0e339030042200e6a545ad
Jack Yeh <jckyeh@gmail.com>
2023-11-11 02:07:18 +0300

    Add nomination form

--- frontend/src/app/nomination/new/page.tsx
+++ frontend/src/app/nomination/new/page.tsx
@@ -3,29 +3,61 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function NewNomination() {
+  const router = useRouter();
   const searchParams = useSearchParams();
   console.log(searchParams);
 
   const groupId = searchParams.get("groupId");
 
-  // const { groupId } = router.query; // Access groupId from the query
+  const handleSubmit = async () => {
+    try {
+      const response = true;
+
+      if (response) {
+        console.log("success submit");
+
+        router.push(`/group/${groupId}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-2xl">Nominate new member</h1>
         {/* TODO: Add form */}
-        <p>Group: {groupId}</p>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <p>Group: {groupId}</p>
+          <label htmlFor="walletAddress">Enter ENS or wallet address:</label>
+          <input
+            type="text"
+            id="walletAddress"
+            name="walletAddress"
+            className="py-3 px-4 rounded bg-green-800 mt-2 mb-6"
+            placeholder="Enter address"
+            required
+          />
+        </form>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+        >
+          Nominate
+        </button>
+        {/* <p>Group: {groupId}</p>
         <p>Enter ENS or wallet address</p>
         <Link
           href={`/group/${groupId}`}
           className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         >
           Nominate
-        </Link>
+        </Link> */}
       </div>
     </main>
   );
