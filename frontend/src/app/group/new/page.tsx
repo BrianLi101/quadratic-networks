84a9b1a9689a02920da3d43d621b9d764b7405f6
Brian Li <brian.li.101@gmail.com>
2023-11-11 08:01:55 +0300

    basic working version of contract deploy

--- frontend/src/app/group/new/page.tsx
+++ frontend/src/app/group/new/page.tsx
@@ -2,17 +2,36 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { useAccount } from 'wagmi';
+import { useAccount, useWalletClient } from 'wagmi';
 import LoadingIndicator from '@/components/LoadingIndicator';
 
-export default function NewGroup() {
+import WagmiProvider from '@/components/WagmiProvider';
+import abi from '@/contracts/QuadraticNetworksNFT/abi.json';
+import bytecode from '@/contracts/QuadraticNetworksNFT/bytecode.json';
+import { goerli } from 'viem/chains';
+function NewGroup() {
   const router = useRouter();
   const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+  const { data: walletClient } = useWalletClient();
   const [deploying, setDeploying] = useState<boolean>(false);
 
   const handleSubmit = async () => {
     console.log('user clicked submit');
+    if (!walletClient) return null;
+
     setDeploying(true);
+    try {
+      const hash = await walletClient.deployContract({
+        abi,
+        bytecode: bytecode.bytecode as `0x${string}`,
+        args: ['Test', 'TEST', [address], 1000],
+        chain: goerli,
+      });
+      setDeploying(false);
+    } catch (error) {
+      console.log(error);
+    }
+    return;
     try {
       const response = true;
       const id = '1';
@@ -54,12 +73,13 @@ export default function NewGroup() {
             />
           </div> */}
         </form>
-        <h1>{deploying}</h1>
+        <h1>deploying: {deploying}</h1>
+        <h1>address: {JSON.stringify(address)}</h1>
         <button
           type="submit"
           onClick={handleSubmit}
           className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-          disabled={!address}
+          disabled={!address || !isConnected || deploying}
         >
           Create group {deploying && <LoadingIndicator />}
         </button>
@@ -67,3 +87,12 @@ export default function NewGroup() {
     </main>
   );
 }
+
+const WrappedNewGroup = () => {
+  return (
+    <WagmiProvider>
+      <NewGroup />
+    </WagmiProvider>
+  );
+};
+export default WrappedNewGroup;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,22 +9,7 @@ import Header from './components/header';
 import { Web3Modal } from '@/context/Web3Modal';
 
 const inter = Inter({ subsets: ['latin'] });
-
-const apiKey = process.env.ALCHEMY_API_KEY_GOERLI;
-if (!apiKey) {
-  throw new Error('ALCHEMY_API_KEY_GOERLI is not defined in the environment.');
-}
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [goerli],
-  [alchemyProvider({ apiKey })]
-);
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
-});
+console.log(JSON.stringify(publicProvider));
 
 export const metadata: Metadata = {
   title: 'ZuGroups',
@@ -36,16 +21,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (typeof window !== 'undefined') {
+    // Code that should only run on the client side
+    return null;
+  }
   return (
     <html lang="en">
-      <WagmiConfig config={config}>
-        <body className={inter.className}>
-          <Web3Modal>
-            <Header />
-            {children}
-          </Web3Modal>
-        </body>
-      </WagmiConfig>
+      <body className={inter.className}>
+        <Web3Modal>
+          <Header />
+          {children}
+        </Web3Modal>
+      </body>
     </html>
   );
 }
